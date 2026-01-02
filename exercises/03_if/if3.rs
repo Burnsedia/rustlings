160addecdeda6ccb810a45b89bb35909ba1c4e38
Burnsedia <bburnsed1995@gmail.com>
2026-01-01 23:32:37 -0500

    wip still not pasing

--- exercises/03_if/if3.rs
+++ exercises/03_if/if3.rs
@@ -1,6 +1,6 @@
 fn animal_habitat(animal: &str) -> &str {
     // TODO: Fix the compiler error in the statement below.
-    let identifier = if animal == "crab" {
+    let identifier if animal == "crab" {
         1.0
     } else if animal == "gopher" {
         2.0
