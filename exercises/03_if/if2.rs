06f0539523f13b2d22934ef7a6aefb4b6bce1567
Burnsedia <bburnsed1995@gmail.com>
2026-01-01 22:57:51 -0500

    wip: getting different effort

--- exercises/03_if/if2.rs
+++ exercises/03_if/if2.rs
@@ -1,6 +1,12 @@
 // TODO: Fix the compiler error on this function.
 fn picky_eater(food: &str) -> &str {
-    if food == "strawberry" { "Yummy!" } else { "1" }
+    if food == "strawberry" {
+        "Yummy!"
+    } else if food == "patato" {
+        "I guess I can eat that."
+    } else {
+        1
+    }
 }
 
 fn main() {
