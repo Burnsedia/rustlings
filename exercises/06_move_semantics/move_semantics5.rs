e011be46711fa2798cf3339b5ef2626dbf79ecae
Burnsedia <bburnsed1995@gmail.com>
2026-01-14 22:42:34 -0500

    I just had to get the syntax

--- exercises/06_move_semantics/move_semantics5.rs
+++ exercises/06_move_semantics/move_semantics5.rs
@@ -9,16 +9,14 @@ fn get_char(data: &String) -> char {
 }
 
 // Should take ownership
-fn string_uppercase(data: &mut String) {
-    *data = data.to_uppercase();
+fn string_uppercase(mut data: String) {
+    data = data.to_uppercase();
 
     println!("{data}");
 }
 
 fn main() {
     let data = "Rust is great!".to_string();
-
-    get_char(&mut data.clone());
-
-    string_uppercase(&data);
+    get_char(&data);
+    string_uppercase(data);
 }
