36e7947a4e4f59025155c99d0e3b457f1ac82566
Burnsedia <bburnsed1995@gmail.com>
2026-01-18 16:39:25 -0500

    this should be good

--- exercises/07_structs/structs1.rs
+++ exercises/07_structs/structs1.rs
@@ -1,13 +1,12 @@
 struct ColorRegularStruct {
     // TODO: Add the fields that the test `regular_structs` expects.
     // What types should the fields have? What are the minimum and maximum values for RGB colors?
-   red: i32,
-   green: i32,
-   blue: i32,
-
+    red: i32,
+    green: i32,
+    blue: i32,
 }
-
-struct ColorTupleStruct(bool,i32);
+//* TODO: Add the fields that the test `tuple_structs` expects */
+struct ColorTupleStruct(i32,i32,i32);
 
 #[derive(Debug)]
 struct UnitStruct;
@@ -27,8 +26,7 @@ mod tests {
             red: 0,
             green: 255,
             blue: 0,
-        };
-
+        }
 
         assert_eq!(green.red, 0);
         assert_eq!(green.green, 255);
@@ -38,7 +36,7 @@ mod tests {
     #[test]
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct.
-        let green = ColorTupleStruct(0, 255);
+        let green = ColorTupleStruct(0, 255, 0);
 
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
