18f4cce1d379b7dc156d7ffc10901b0c5a47cb14
Burnsedia <bburnsed1995@gmail.com>
2026-01-19 00:55:57 -0500

    this should pass

--- exercises/07_structs/structs1.rs
+++ exercises/07_structs/structs1.rs
@@ -45,7 +45,7 @@ mod tests {
     #[test]
     fn unit_structs() {
         // TODO: Instantiate a unit struct.
-        // let unit_struct =
+        let unit_struct = UnitStruct();
         let message = format!("{unit_struct:?}s are fun!");
 
         assert_eq!(message, "UnitStructs are fun!");
