ef00e963b6827350273243cb489c3d10353af47d
Burnsedia <bburnsed1995@gmail.com>
2026-01-20 22:19:55 -0500

    I found a logic bug, no "Memroy Safe" langue can save you from that fuckuip

--- exercises/07_structs/structs3.rs
+++ exercises/07_structs/structs3.rs
@@ -28,7 +28,7 @@ impl Package {
         // TODO: Read the tests that use this method to find out when a package
         // is considered international.
         if self.sender_country == self.recipient_country {
-            return false;
+            return true;
         } else {
             return false;
         }
