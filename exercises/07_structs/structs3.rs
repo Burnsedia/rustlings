c8377abd78cd03c40f0fbf2342a0978624fa2d5c
Burnsedia <bburnsed1995@gmail.com>
2026-01-20 22:06:53 -0500

    Rusts syntax is confusing, you can return without a return statment

--- exercises/07_structs/structs3.rs
+++ exercises/07_structs/structs3.rs
@@ -32,7 +32,7 @@ impl Package {
     // TODO: Add the correct return type to the function signature.
     fn get_fees(&self, cents_per_gram: u32) {
         // TODO: Calculate the package's fees.
-        self.weight_in_grams * cents_per_gram
+        self.weight_in_grams * cents_per_gram;
     }
 }
 
