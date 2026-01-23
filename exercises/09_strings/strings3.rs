8b9fe7298c10cfac3bd4b88056dc5c3dbfce2e4b
Burnsedia <bburnsed1995@gmail.com>
2026-01-22 22:02:05 -0500

    getting concatenations working

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -5,10 +5,13 @@ fn trim_me(input: &str) -> &str {
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    let world= " world!"
+    return input + world;
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    
 }
 
 fn main() {
