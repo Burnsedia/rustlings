762eb1b2a81803b0f14476bb3bdd7f89cffb8dae
Burnsedia <bburnsed1995@gmail.com>
2026-07-10 22:33:20 -0400

    I need to relearn and start over

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,7 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Command) -> ??? { ??? }
 }
 
 fn main() {
